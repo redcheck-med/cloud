21ec81278a5a00faf2ce8022bf45110bf11a9536
Weverton Marques <wevertonms@gmail.com>
2022-06-22 15:13:21 -0300

    Novas variáveis WINSCP_PATH e SYNC_OPTIONS

--- sync_ftp.cmd
+++ sync_ftp.cmd
@@ -2,8 +2,8 @@
 set USERNAME=""
 set CLINICAID=""
 set ORIGIN=""
-set WINSCP_PATH="C:\Program Files (x86)\WinSCP\WinSCP.com"
-set SYNC_OPTIONS="-nopreservetime -nopermissions"
+set WINSCP_PATH="WinSCP.com"
+set SYNC_OPTIONS="-nopreservetime"
 
 "%WINSCP_PATH" ^
     /log="logs/%date:/=_%.log" /ini=WinSCP.ini ^
@@ -11,7 +11,7 @@ set SYNC_OPTIONS="-nopreservetime -nopermissions"
     "open %USERNAME%@sftp.redcheck.com.br -privatekey=C:\Path\to\file.ppk" ^
     "option batch continue" ^
     "synchronize remote %ORIGIN% /ftp-img-storage/clinicaid=%CLINICAID% -criteria=size %SYNC_OPTIONS%" ^
-    "keepuptodate %ORIGIN% /ftp-img-storage/clinicaid=%CLINICAID% %SYNC_OPTIONS%" ^
+    "keepuptodate %ORIGIN% /ftp-img-storage/clinicaid=%CLINICAID% -nopermissions %SYNC_OPTIONS%" ^
     "exit"
 
 exit /b %ERRORLEVEL%
