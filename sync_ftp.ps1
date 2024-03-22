21ec1f35b4290087fb0f245c6918c72b67ab1a15
Weverton Marques <wevertonms@gmail.com>
2024-03-22 15:04:56 -0300

    🔨 Atualiza script powershell

--- sync_ftp.ps1
+++ sync_ftp.ps1
@@ -1,32 +1,30 @@
 # Needed for PowerShell 7.3 and newer
 $PSNativeCommandArgumentPassing = "Legacy"
-$USERNAME="ds4"
-$CLINICA_ID="244"
-$PASSWORD=""
-$FTP_HOST="ftp.redcheck.com.br"
+$USERNAME = ""
+$CLINICA_ID = ""
+$FTP_HOST = "ftp.redcheck.com.br"
+# $PASSWORD = "" # Use apenas de não usar $SITE_NAME
 # $SITE_NAME="ftp://${USERNAME}:${PASSWORD}@${FTP_HOST}/"
-$SITE_NAME="${USERNAME}@${FTP_HOST}"
-$ORIGIN="${Env:USERPROFILE}\Documents\DS4\Redcheck\redcheck_s3_sync\local\clinicaId=244"
-$WINSCP_COM_PATH="${Env:USERPROFILE}\AppData\Local\Programs\WinSCP\WinSCP.com"
-$SYNC_OPTIONS="-nopreservetime"
+$SITE_NAME = "${USERNAME}@${FTP_HOST}"
+$ORIGIN = "${Env:USERPROFILE}\Documents\DS4\Redcheck\redcheck_s3_sync\local\clinicaId=${CLINICA_ID}"
+$WINSCP_COM_PATH = "${Env:USERPROFILE}\AppData\Local\Programs\WinSCP\WinSCP.com"
+$SYNC_OPTIONS = "-nopreservetime"
 
 
 & "$WINSCP_COM_PATH" `
   /command `
-    "open $SITE_NAME" `
-    "option batch continue" `
-    "synchronize remote ${ORIGIN} / -criteria=date ${SYNC_OPTIONS}" `
-    "keepuptodate ${ORIGIN} / -nopermissions ${SYNC_OPTIONS}" `
-    "exit"
+  "open $SITE_NAME" `
+  "option batch continue" `
+  "synchronize remote ${ORIGIN} / -criteria=date ${SYNC_OPTIONS}" `
+  "keepuptodate ${ORIGIN} / -nopermissions ${SYNC_OPTIONS}" `
+  "exit"
 
 
 $winscpResult = $LastExitCode
-if ($winscpResult -eq 0)
-{
+if ($winscpResult -eq 0) {
   Write-Host "Success"
 }
-else
-{
+else {
   Write-Host "Error"
 }
 
